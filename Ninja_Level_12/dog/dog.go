334421ec634623eb2f45d0fb824fdbe8042e7588
Hemanth Savasere <hemanth.savasere@synapse.com>
2018-12-24 12:32:38 +0530

    Ninja Level 12

---
+++ Ninja_Level_12/dog/dog.go
@@ -0,0 +1,8 @@
+// Package dog for understanding Dog's Loyalty
+package dog
+
+// Years of dog package converting human years into dog years
+func Years(year int) int {
+	yearsdog := year * 7
+	return yearsdog
+}
---
+++ Ninja_Level_12/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"GoCourse/Ninja_Level_12/dog"
+	"fmt"
+)
+
+func main() {
+	dogs := dog.Years(10)
+	fmt.Println("Hello", dogs)
+
+}
